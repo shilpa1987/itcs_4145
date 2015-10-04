4e4652641d1d49d2a0b1aa83542462e1e5a19b99
Nicholas Wade <nwade514@gmail.com>
2015-10-03 21:33:08 -0400

    started work on assignment_3

--- assignment_2/static_heat.c
+++ assignment_2/static_heat.c
@@ -1,30 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <omp.h>
+#include "X11Macros.h"
 
-#define N 100
-#define T 5000
+#define X_RESN 1000
+#define Y_RESN 1000
 
 int main () {
-	int i, j, iteration;
-	double h[2][N][N];
+	int i, j, iteration, T, N;
+	double h[2][256][256], g[2][256][256];
 	int current = 0;
 	int next = 1;
-	double start, end, par_time, seq_time;
+	double seq_start, seq_end, par_start, par_end;
+	
+	initX11(X_RESN, Y_RESN);
+	
+	printf("How big is the room?\n");
+	scanf("%d", &N);
+	
+	printf("How many iterations?\n");
+	scanf("%d", &T);
 	
 	for (i = 0; i < N; ++i) {
 		for (j = 0; j < N; ++j) {
-			if (i == 0 && (j > 39 && j < 60)) {
+			if (i == 0 && (j > N * 0.30 && j < N * 0.70)) {
 				h[0][i][j] = 100;
 				h[1][i][j] = 100;
+				g[0][i][j] = 100;
+				g[1][i][j] = 100;
 			} else {
 				h[0][i][j] = 20;
 				h[1][i][j] = 20;
+				g[0][i][j] = 20;
+				g[1][i][j] = 20;
 			}
 		}
 	}
 	
-	printf("Initial Values:\n");
+	printf("Sequential Initial Values:\n");
 	for (i = 0; i < N; ++i) {
 		for (j = 0; j < N; ++j) {
 			if(j % 10 == 0 && i % 10 == 0)
@@ -34,8 +47,9 @@ int main () {
 			printf ("\n");
 	}
 	
-	start = omp_get_wtime();
+	seq_start = omp_get_wtime();
 	for (iteration = 0; iteration < T; iteration++) {
+		XSetForeground(display, gc, BLUE);
  		for (i = 1; i < N - 1; i++)
  			for (j = 1; j < N - 1; j++) {
 				h[next][i][j] = 0.25 * (h[current][i + 1][j] + h[current][i - 1][j] + h[current][i][j - 1]  + h[current][i][j + 1]);
@@ -43,10 +57,9 @@ int main () {
  		current = next;
  		next = 1 - current;
  	}
- 	end = omp_get_wtime();
- 	seq_time = end - start;
-	
-	printf ("\nSequential final values:\n");
+ 	seq_end = omp_get_wtime();
+ 	
+	printf("\nSequential Final Values\n");
 	for (i = 0; i < N; ++i) {
 		for (j = 0; j < N; ++j) {
 			if(j % 10 == 0 && i % 10 == 0)
@@ -56,44 +69,52 @@ int main () {
 			printf ("\n");
 	}
 	
-	//reset the values
+	printf("\nParallel Initial Values\n");
 	for (i = 0; i < N; ++i) {
 		for (j = 0; j < N; ++j) {
-			if (i == 0 && (j > 39 && j < 60)) {
-				h[0][i][j] = 100;
-				h[1][i][j] = 100;
-			} else {
-				h[0][i][j] = 20;
-				h[1][i][j] = 20;
-			}
+			if(j % 10 == 0 && i % 10 == 0)
+				printf ("%.2f\t", g[current][i][j]);
 		}
+		if(i % 10 == 0)
+			printf ("\n");
 	}
 	
-	//TODO: parallelize this bitch
-	start = omp_get_wtime();
+	par_start = omp_get_wtime();
 	for (iteration = 0; iteration < T; iteration++) {
+		#pragma omp parallel for private (i, j)
  		for (i = 1; i < N - 1; i++)
+ 			#pragma omp parallel for private (j)
  			for (j = 1; j < N - 1; j++) {
-				h[next][i][j] = 0.25 * (h[current][i + 1][j] + h[current][i - 1][j] + h[current][i][j - 1]  + h[current][i][j + 1]);
+				g[next][i][j] = 0.25 * (g[current][i + 1][j] + g[current][i - 1][j] + g[current][i][j - 1]  + g[current][i][j + 1]);
 			}
  		current = next;
  		next = 1 - current;
  	}
-	end = omp_get_wtime();
-	par_time = end - start;
-	
-	printf ("\nSequential final values:\n");
+ 	par_end = omp_get_wtime();
+ 	
+	printf("\nSequential Final Values\n");
 	for (i = 0; i < N; ++i) {
 		for (j = 0; j < N; ++j) {
 			if(j % 10 == 0 && i % 10 == 0)
-				printf ("%.2f\t", h[current][i][j]);
+				printf ("%.2f\t", g[current][i][j]);
 		}
 		if(i % 10 == 0)
 			printf ("\n");
 	}
 	
-	printf ("Sequential time: %f\n", seq_time);
-	printf ("Parallel Time: %f\n", par_time);
+	int error = 0;
+	for (i = 0; i < N; ++i)
+	{
+		for (j = 0; j < N; ++j)
+		{
+			if ((h[current][i][j] - g[current][i][j] > 0.001) || (h[current][i][j] - g[current][i][j] > 0.001))
+				error = -1;
+		}
+	}
+	if(error == -1) {printf("ERROR, sequential and parallel versions give different answers\n"); }
+	
+	printf("\nsequential time: %f\n", seq_end - seq_start);
+	printf("parallel time: %f\n", par_end - par_start);
 	
 	return 0;		
 }
